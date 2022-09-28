d6398c91653d860494e34fc7246147f183143568
rihan_a <a.rihan@live.com>
2022-09-28 16:21:29 +0200

    control-flow-exercise

---
+++ week1/day2/control-flow/control-flow-exercise/control-flow-exercise.js
@@ -0,0 +1,27 @@
+// part 01
+function logType(input) {
+    console.log(typeof input + "!");
+}
+
+logType("hello");
+logType({ name: "name", age: 25 });
+
+// part 02
+var a = {
+    Berlin: "Germany",
+    Paris: "France",
+    "New York": "USA",
+};
+
+let b = {};
+
+for (let value in a) {
+    //console.log(value + a[value]);
+    b[a[value]] = value;
+}
+console.log(b);
+
+// part 03
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
