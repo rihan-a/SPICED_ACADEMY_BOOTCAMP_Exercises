cb2d1a48fad5b7453482dc8ed6221e62e64b4110
rihan_a <a.rihan@live.com>
2022-09-29 13:26:58 +0200

    functions and scopes exercise

---
+++ week1/day3/functions-and-scopes/functions-scopes-exercise.js
@@ -0,0 +1,35 @@
+// part 1
+function sum() {
+    let totalSum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        totalSum += arguments[i];
+    }
+    return totalSum;
+}
+let result = sum(5, 10, 15, 100, 200);
+console.log(result);
+
+// part 2
+function waitThenRun(wait) {
+    setTimeout(wait, 1500);
+}
+
+waitThenRun(function () {
+    console.log("Hello!");
+    waitThenRun(function () {
+        console.log("Goodbye!");
+    });
+});
+
+// part 3
+function check(num) {
+    if (num < 0 || num === 0 || isNaN(num)) {
+        return "ERROR";
+    } else if (num >= 1000000) {
+        return num;
+    } else {
+        return check(num * 10);
+    }
+}
+
+console.log(check(10));
