324f82e7c11fcd337061b515239e9dba96a43de9
rihan_a <a.rihan@live.com>
2022-09-30 16:15:15 +0200

    fix part01 in control flow exercise

--- week1/day2/control-flow/control-flow-exercise/control-flow-exercise.js
+++ week1/day2/control-flow/control-flow-exercise/control-flow-exercise.js
@@ -1,16 +1,31 @@
 // part 01
 function logType(input) {
     if (Array.isArray(input)) {
-        console.log("Array!");
+        console.log("array!");
+    } else if (
+        isNaN(input) &&
+        typeof input != "string" &&
+        typeof input != "object" &&
+        input !== undefined
+    ) {
+        console.log("NaN!");
+    } else if (Object.is(input, null)) {
+        console.log("null!");
+    } else if (typeof input === "undefined") {
+        console.log("undefined!");
     } else {
         console.log(typeof input + "!");
     }
 }
 
+// Part 01 test
 logType("hello");
 logType({ name: "name", age: 25 });
 logType(200);
 logType([1, 2, 3]);
+logType(NaN);
+logType(null);
+logType(undefined);
 
 // part 02
 var a = {
