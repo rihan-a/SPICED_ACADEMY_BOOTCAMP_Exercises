5d956b6ecf4dd16cd059848506a2d139c98d208d
rihan_a <a.rihan@live.com>
2022-10-04 15:10:48 +0200

    constructors exercise part02

--- week2/day1/constructors/constructors-exercise/constructors-exercise.js
+++ week2/day1/constructors/constructors-exercise/constructors-exercise.js
@@ -21,3 +21,21 @@ let rect = new Rectangle(4, 5);
 console.log(rect.getArea());
 let square = new Square(4);
 console.log(square.getArea());
+
+// Part02
+function invertcase(str) {
+    let strArray = str.split("");
+    console.log(strArray);
+    for (let i = 0; i < strArray.length; i++) {
+        if (strArray[i] == strArray[i].toUpperCase()) {
+            strArray[i] = strArray[i].toLowerCase();
+        } else {
+            strArray[i] = strArray[i].toUpperCase();
+        }
+    }
+    let arrStr = strArray.join("");
+    return arrStr;
+}
+
+// Part02 test
+console.log(invertcase("RiHan%1$"));
