f2faa4a7b40312343b3b8b5eca49ba92c5f8524c
rihan_a <a.rihan@live.com>
2022-10-07 16:31:05 +0200

    file cleaning

--- week1/html-css-lab/script.js
+++ week1/html-css-lab/script.js
@@ -5,9 +5,18 @@ const overlayBackground = document.querySelector("#overlay");
 
 hamburgerMenu.addEventListener("click", openSideMenu);
 
-// function to move the aside menu and display the overlay background
+// function to open the aside menu and display the overlay background
 function openSideMenu() {
-    console.log("clicked");
     sideMenuContainer.classList.remove("move-right");
     overlayBackground.style.display = "block";
+}
+
+closeMenu.addEventListener("click", closeSideMenu);
+//close side menu if pressed anywhere outside the sidebar
+overlayBackground.addEventListener("click", closeSideMenu);
+
+// function to close the aside menu and disable the overlay background
+function closeSideMenu() {
+    sideMenuContainer.classList.add("move-right");
+    overlayBackground.style.display = "none";
 }
