c6a6c3aaf80c7961208ddebe2cd53cd4c50c96fc
rihan_a <a.rihan@live.com>
2022-10-12 12:12:31 +0200

    refactoring the code using jQuery

--- week2/ticker/script.js
+++ week2/ticker/script.js
@@ -1,32 +1,46 @@
 // IIFE
 (function () {
-    let headlineContainer = document.querySelector("#headlines");
-    let headlinesArr = document.querySelectorAll(".headline-link");
+    // Variables
+    //let headlineContainer = document.querySelector("#headlines");
+    let $headlineContainer = $("#headlines");
 
-    let linkWidth = headlineContainer.firstElementChild.offsetWidth;
-    let linkOffsetLeft = headlineContainer.firstElementChild.offsetLeft;
+    //let headlinesArr = document.querySelectorAll(".headline-link");
+    let $headlinesArr = $(".headline-link");
+
+    let linkWidth = $headlinesArr[0].offsetWidth;
+
+    let linkOffsetLeft = $headlinesArr[0].offsetLeft;
     let requestID;
+
     function move() {
         if (linkOffsetLeft === linkWidth * -1) {
             console.log("now shift");
             // move the first link to the end of the array
-            headlineContainer.appendChild(headlineContainer.firstElementChild);
+            $headlineContainer[0].appendChild($headlinesArr[0]);
             // update the offsetWidth value
-            linkOffsetLeft = headlineContainer.firstElementChild.offsetLeft;
+            linkOffsetLeft = $headlinesArr[0].offsetLeft;
         }
 
-        linkOffsetLeft = linkOffsetLeft - 2;
-        headlineContainer.style.left = `${linkOffsetLeft}px`;
+        linkOffsetLeft--;
+        //$headlineContainer[0].style.left = `${linkOffsetLeft}px`;
+        $($headlineContainer[0]).css("left", linkOffsetLeft);
 
         requestID = requestAnimationFrame(move);
     }
 
     requestAnimationFrame(move);
 
-    for (let i = 0; i < headlinesArr.length; i++) {
-        headlinesArr[i].addEventListener("mouseover", stopTicker);
-        headlinesArr[i].addEventListener("mouseleave", runTicker);
-    }
+    // for (let i = 0; i < headlinesArr.length; i++) {
+    //     headlinesArr[i].addEventListener("mouseover", stopTicker);
+    //     headlinesArr[i].addEventListener("mouseleave", runTicker);
+    // }
+
+    $headlinesArr.mouseover(function () {
+        stopTicker();
+    });
+    $headlinesArr.mouseleave(function () {
+        runTicker();
+    });
 
     // function to stop the ticker
     function stopTicker() {
