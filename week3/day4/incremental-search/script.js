12ebc2191471c61e2d6232ecea2dd6db5bf378db
rihan_a <a.rihan@live.com>
2022-10-13 17:18:39 +0200

    functions updated

--- week3/day4/incremental-search/script.js
+++ week3/day4/incremental-search/script.js
@@ -205,22 +205,20 @@ let filteredCountries;
 
 let searchInputValue = "";
 
+const $clearSearchBtn = $(".clear-search");
+
 // empty the search input when refreshed
 $searchInput.val("");
 
-$searchInput.keyup(function () {
+// triger the filter function on input event
+$searchInput.on("input", function () {
     searchInputValue = $searchInput.val().toLowerCase();
-    filterCountriesArr(searchInputValue);
+    filter(searchInputValue);
     console.log(searchInputValue);
 });
 
-//let testCountry = "egypt";
-
 // function to search and filter using the user input and returns list of countries as a result
-function filterCountriesArr(searchInputValue) {
-    // console.log(
-    //     testCountry.slice(0, searchInputValue.length) === searchInputValue
-    // );
+function filter(searchInputValue) {
     console.log(searchInputValue);
     if (searchInputValue != "") {
         filteredCountries = countries.filter((country) => {
@@ -238,13 +236,17 @@ function filterCountriesArr(searchInputValue) {
 // function to print the results
 function printSearchResults(filteredCountries) {
     $searchResults.html("");
-    if (searchInputValue != "") {
+    if (searchInputValue !== "") {
         filteredCountries.forEach((country, id) => {
             if (id < 4) {
-                $searchResults[0].innerHTML += `<li>${country}</li>`;
-                //$searchResults.html(`<li>${country}</li>`);
+                $searchResults[0].innerHTML += `<li class="result">${country}</li>`;
+                //$searchResults.html(`<li >${country}</li>`);
             }
         });
+    }
+
+    if (filteredCountries.length == 0) {
+        $searchResults[0].innerHTML += `<li>No results..</li>`;
     }
 }
 
@@ -253,11 +255,11 @@ $searchResults.fadeOut(100);
 
 // fadeout results when clicked outside the search bar
 $searchInput.blur(() => {
-    $searchResults.fadeOut(200);
+    $searchResults.fadeOut(300);
 });
 // fadein results when clicked inside the search bar
 $searchInput.focus(() => {
-    $searchResults.fadeIn(200);
+    $searchResults.fadeIn(300);
 });
 
 // show the country name in search bar when clicked
@@ -266,3 +268,23 @@ $searchResults.click((e) => {
     let currentSelection = e.target.innerText;
     $searchInput.val(currentSelection);
 });
+
+// clear search input
+$clearSearchBtn.click(() => {
+    $searchInput.val("");
+});
+
+// highlight a result by keyboard arrows -- not working yet --
+document.onkeydown = (e) => {
+    if (e.key == "ArrowDown") {
+        if ($(".higlight")) {
+            console.log("there is one highlighted");
+            console.log($(".result").next().first());
+            $(".result").next().first().addClass("higlight");
+        }
+        $(".result").first().addClass("highlight");
+
+        //$(".higlight").removeClass("higlight");
+    } else if (e.key == "ArrowUp") {
+    }
+};
