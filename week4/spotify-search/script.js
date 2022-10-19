eccf6ce89372316a43d7b5c700514a7b487461cc
rihan_a <a.rihan@live.com>
2022-10-19 18:25:09 +0200

    check for images and no results logic done

--- week4/spotify-search/script.js
+++ week4/spotify-search/script.js
@@ -6,15 +6,21 @@
     let resultsContainer = document.querySelector(".results-container");
     let searchTitle = document.querySelector(".search-title");
 
-    searchBtn.addEventListener("click", () => {
-        resultsContainer.innerHTML = "";
+    searchBtn.addEventListener("click", searchMusic);
+    document.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            searchMusic();
+        }
+    });
 
+    function searchMusic() {
+        resultsContainer.innerHTML = "";
         let inputValue = searchInput.value;
         let typeValue = searchType.value;
         // console.log(inputValue);
         // console.log(typeValue);
         getMusic(inputValue, typeValue);
-    });
+    }
 
     // fetch data from Spotify
     function getMusic(name, type) {
@@ -22,11 +28,12 @@
             url: `https://spicedify.herokuapp.com/spotify?q=${name}&type=${type}`,
             method: "GET", // its get by default
             success: function (data) {
+                console.log(data);
                 let musicData = data.artists.items || data.albums.items;
 
                 if (musicData.length > 0 && musicData.length != 0) {
-                    searchTitle.innerText = `Search results for ${name}`;
-                    printSearchResults(musicData);
+                    searchTitle.innerText = `Search results for '${name}'`;
+                    printSearchResults(musicData, type);
                 } else {
                     searchTitle.innerText = `No results`;
                 }
@@ -39,20 +46,29 @@
         });
     }
 
-    function printSearchResults(musicData) {
+    function printSearchResults(musicData, type) {
+        let imgUrl;
         musicData.forEach((item) => {
-            resultsContainer.innerHTML += `
+            // check if there is images otherwise show and empty placeholder
+            if (type == "artist") {
+                if (item.images.length > 0) {
+                    imgUrl = item.images[0].url;
+                } else {
+                    imgUrl =
+                        "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+                }
+                resultsContainer.innerHTML += `
          <div class="outer-card">
                     <div class="inner-card">
                         <div class="card-img">
-                            <img src="${item.images[0].url}" alt="" />
+                            <img src="${imgUrl}" alt="" />
                         </div>
                         <div class="card-desc">
                             <a href="#">${item.name}</a>
                             <div class="artist-play-container">
                                 <p>${item.genres[0]}</p>
                                 <div class="play-btn">
-                                    <a href="${item.external_urls.spotify}">
+                                    <a href="${item.external_urls.spotify}" target="_blank">
                                         <img
                                         src="./assets/play_arrow_FILL1_wght400_GRAD0_opsz48.svg"
                                         alt=""
@@ -64,6 +80,31 @@
                     </div>
                 </div>
         `;
+            } else {
+                resultsContainer.innerHTML += `
+         <div class="outer-card">
+                    <div class="inner-card">
+                        <div class="card-img">
+                            <img src="${item.images[0].url}" alt="" />
+                        </div>
+                        <div class="card-desc">
+                            <a href="#">${item.name}</a>
+                            <div class="artist-play-container">
+                                <p>${item.genres[0]}</p>
+                                <div class="play-btn">
+                                    <a href="${item.external_urls.spotify}" target="_blank">
+                                        <img
+                                        src="./assets/play_arrow_FILL1_wght400_GRAD0_opsz48.svg"
+                                        alt=""
+                                        />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+            }
         });
     }
 })();
